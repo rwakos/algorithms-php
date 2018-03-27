1da9eddeb73b9a6aa494c5bc9051168311b91367
Richardr123 <rwakos@gmail.com>
2018-03-27 12:03:50 -0400

    LinkedList Full

--- App/Models/LinkedList.php
+++ App/Models/LinkedList.php
@@ -15,15 +15,175 @@ class LinkedList
     }
 
     function size() {
-    $counter = 0;
-    $node = $this->head;
+        $counter = 0;
+        $node = $this->head;
 
         while ($node) {
             $counter++;
             $node = $node->next;
         }
 
-    return $counter;
+        return $counter;
+    }
+
+    function getFirst() {
+        return $this->head;
+    }
+
+    function getLast() {
+        $node = $this->head;
+
+        if ($node===null){
+            return null;
+        }
+
+        while ($node) {
+            if ($node->next===null){
+                return $node;
+            }
+            $node = $node->next;
+        }
+        return $node;
+    }
+
+    function clear()
+    {
+        $this->head = null;
+    }
+
+    function removeFirst(){
+        if ($this->head===null){
+            return;
+        }
+
+        $this->head = $this->head->next;
+    }
+
+    function RemoveLast()
+    {
+        $previous = $this->head;
+
+        if ($previous === null) {
+            return;
+        }
+
+        if ($previous->next === null) {
+            $this->head = null;
+            return;
+        }
+
+        $node = $previous->next;
+        while ($node !== null) {
+            $previous = $node;
+            $node = $node->next;
+        }
+
+        $previous->next = null;
+    }
+
+    function InsertLast($data){
+        $node = new Node($data);
+        $lnode = $this->getLast();
+
+        if ($lnode===null){
+            $this->head = $node;
+        } else {
+            $lnode->next = $node;
+        }
+    }
+
+    /*
+     * Returns a node at index
+     * */
+    function getAt($index){
+        if (!is_numeric($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if (!is_integer($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if ($index < 0){
+            throw new \InvalidArgumentException;   //Or you could swapp $index to cero...
+        }
+
+        if ($this->head===null){
+            return null;
+        }
+        $node = $this->head;
+
+        $i = -1;
+        while ($node) {
+            $i++;
+            if ($i===$index){
+                return $node;
+            }
+            $node = $node->next;
+        }
+
+        return null;
+    }
+
+    /*
+     * Remove a node at index
+     * */
+    function removeAt($index){
+        if (!is_numeric($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if (!is_integer($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if ($index < 0){
+            throw new \InvalidArgumentException;   //Or you could swapp $index to cero...
+        }
+
+        if ($this->head===null){
+            return;
+        }
+
+        //Validate when index = 0
+        if (($index===0)&&($this->head !== null)){
+            $this->head = $this->head->next;
+
+        } elseif (($index===0)&&($this->head === null)){
+            $this->head = null;
+        } else {
+            $node = $this->getAt($index-1);
+            $toRemove = $node->next;
+            $node->next = $toRemove->next;
+        }
+    }
+
+    /*
+     * Remove a node at index
+     * */
+    function insertAt($index, $data){
+        if (!is_numeric($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if (!is_integer($index)){
+            throw new \InvalidArgumentException;
+        }
+
+        if ($index < 0){
+            throw new \InvalidArgumentException;   //Or you could swapp $index to cero...
+        }
+
+        //Validate when index = 0
+        if ($index===0){
+            $this->insertFirst($data);
+        } else {
+            $new = new Node($data);
+            $previous = $this->getAt($index-1);
+            $current = $previous->next;
+            $previous->next = $new;
+            $new->next = $current;
+        }
     }
 }
 
--- tests/unit/LinkedListTest.php
+++ tests/unit/LinkedListTest.php
@@ -1,6 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use App\Models\LinkedList;
+use App\Models\Node;
 
 class LinkedListTest extends TestCase
 {
@@ -10,10 +11,157 @@ class LinkedListTest extends TestCase
     }
 
     public function testCreateLinkedList(){
-        $a = New LinkedList();
-        $this->assertEquals(null,$a->head);
+        $l = New LinkedList();
+        $this->assertEquals($l->head, null);
     }
 
+    public function testInsertFirst(){
+        $a = 'a';
+        $l = New LinkedList();
+        $l->insertFirst($a);
+        $this->assertEquals($a, $l->head->data);
+    }
 
+    public function testGetFirst(){
+        $a = 'a';
+        $l = New LinkedList();
+        $l->insertFirst($a);
+        $node = $l->getFirst();
+        $this->assertEquals($a,$node->data);
+    }
+
+    public function testGetLastWhenEmptyList(){
+        $l = New LinkedList();
+        $node = $l->getLast();
+        $this->assertEquals(null, $node);
+    }
+
+    public function testGetLast(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($b);
+        $l->insertFirst($c);
+        $l->insertFirst($a);
+
+        $node = $l->getLast();
+        $this->assertEquals($b, $node->data);
+    }
+
+    public function testClearList(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($b);
+        $l->insertFirst($c);
+        $l->insertFirst($a);
+
+        $l->clear();
+        $this->assertEquals(null,$l->head);
+    }
+
+    public function testRemoveFirst(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($b);
+        $l->insertFirst($c);
+        $l->insertFirst($a);
+        $l->removeFirst();
+        $this->assertEquals($c, $l->head->data);
+    }
+
+    public function testRemoveLast(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($b);
+        $l->insertFirst($c);
+        $l->insertFirst($a);
+        $l->removeLast();
+        $node = $l->getLast();
+        $this->assertEquals($b, $node->data);
+    }
+
+    public function testInsertLast(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($c);
+        $l->insertFirst($b);
+        $l->insertLast($a);
+        $node = $l->getLast();
+        $this->assertEquals($a, $node->data);
+    }
+
+    public function testInsertLastOnEmpty(){
+        $l = New LinkedList();
+        $a = 'a';
+        $l->insertLast($a);
+        $node = $l->getLast();
+        $this->assertEquals($a, $node->data);
+    }
+
+    public function testGetAt(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($c);
+        $l->insertFirst($b);
+        $l->insertLast($a);
+        $node = $l->getAt(2);
+        $this->assertEquals($a, $node->data);
+        $node = $l->getAt(1);
+        $this->assertEquals($c, $node->data);
+        $node = $l->getAt(0);
+        $this->assertEquals($b, $node->data);
+    }
+
+    public function testRemoveAt(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $l->insertFirst($c);
+        $l->insertFirst($b);
+        $l->insertLast($a);
+        $l->removeAt(2);
+        $node = $l->getAt(2);
+        $this->assertEquals(null, $node);
+        $l->insertLast($a);
+        $l->removeAt(1);
+        $node = $l->getAt(1);
+        $this->assertEquals($a, $node->data);
+    }
+
+    public function testInsertAt(){
+        $l = New LinkedList();
+        $a = 'a';
+        $b = 'b';
+        $c = 'c';
+        $d = 'd';
+
+        $l->insertFirst($c);
+        $l->insertFirst($a);
+
+        $l->insertAt(1,$b);
+        $node = $l->getAt(1);
+        $this->assertEquals($b, $node->data);
+
+        $node = $l->getAt(2); //Validate that the list wasnt broken...
+        $this->assertEquals($c, $node->data);
+
+        $l->insertAt(2,$d);
+        $node = $l->getAt(2);
+        $this->assertEquals($d, $node->data);
+
+
+    }
 
 }
